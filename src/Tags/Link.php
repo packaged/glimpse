f56ef65b6d60d7c516416e523380897822c1c398
Brooke Bryan <brooke@bajb.net>
2014-10-15 12:25:14 +0100

    New Link tag

---
+++ src/Tags/Link.php
@@ -0,0 +1,98 @@
+<?php
+namespace Packaged\Glimpse\Tags;
+
+use Packaged\Glimpse\Core\HtmlTag;
+
+class Link extends HtmlTag
+{
+  protected $_tag = 'a';
+
+  public function __construct($uri)
+  {
+    $this->setAttribute('href', $uri);
+  }
+
+  /**
+   * The location specifies the URL of the page the link goes to.
+   *
+   * @param $uri
+   *
+   * @return $this
+   */
+  public function setLocation($uri)
+  {
+    return $this->setAttribute('href', $uri);
+  }
+
+  /**
+   * The target attribute specifies where to open the linked document.
+   *
+   * @param string $target
+   *
+   * @return $this
+   */
+  public function setTarget($target = '_blank')
+  {
+    return $this->setAttribute('target', $target);
+  }
+
+  /**
+   * Set the link to open in a new window or tab
+   *
+   * @return $this
+   */
+  public function forNewWindow()
+  {
+    return $this->setTarget('_blank');
+  }
+
+  /**
+   * The download attribute specifies that the target will be downloaded
+   * when a user clicks on the hyperlink.
+   *
+   * This attribute is only used if the href attribute is set.
+   *
+   * The value of the attribute will be the name of the downloaded file.
+   * There are no restrictions on allowed values, and the browser will
+   * automatically detect the correct file extension and add it to the
+   * file (.img, .pdf, .txt, .html, etc.).
+   *
+   * If the value is omitted, the original href is used.
+   *
+   * @param $location
+   *
+   * @return $this
+   */
+  public function setDownload($location = null)
+  {
+    return $this->setAttribute('download', $location);
+  }
+
+  /**
+   * The rel attribute specifies the relationship between
+   * the current document and the linked document.
+   *
+   * Only used if the href attribute is present.
+   *
+   * alternate  Links to an alternate version of the document
+   * author  Links to the author of the document
+   * bookmark  Permanent URL used for bookmarking
+   * help  Links to a help document
+   * license  Links to copyright information for the document
+   * next  The next document in a selection
+   * nofollow  Links to an unendorsed document, like a paid link.
+   * noreferrer  Specifies that the browser should not send a HTTP referer header
+   * prefetch  Specifies that the target document should be cached
+   * prev  The previous document in a selection
+   * search  Links to a search tool for the document
+   * tag  A tag (keyword) for the current document
+   *
+   * @param string $relation
+   *
+   * @return $this
+   */
+  public function setRel($relation = 'noreferrer')
+  {
+    return $this->setAttribute('rel', $relation);
+  }
+}
---
+++ tests/Tags/LinkTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace Packaged\Tests\Glimpse\Tags;
+
+use Packaged\Glimpse\Tags\Link;
+
+class LinkTest extends \PHPUnit_Framework_TestCase
+{
+  public function testCreate()
+  {
+    $link = new Link('http://www.test.com');
+    $this->assertEquals('<a href="http://www.test.com"></a>', $link->asHtml());
+  }
+
+  public function testNewWindow()
+  {
+    $link = new Link('http://www.test.com');
+    $link->forNewWindow();
+    $this->assertEquals(
+      '<a href="http://www.test.com" target="_blank"></a>',
+      $link->asHtml()
+    );
+  }
+
+  public function testTarget()
+  {
+    $link = new Link('http://www.test.com');
+    $link->setTarget('random');
+    $this->assertEquals(
+      '<a href="http://www.test.com" target="random"></a>',
+      $link->asHtml()
+    );
+  }
+
+  public function testLocation()
+  {
+    $link = new Link('http://www.test.com');
+    $link->setLocation('/here');
+    $this->assertEquals('<a href="/here"></a>', $link->asHtml());
+  }
+
+  public function testRel()
+  {
+    $link = new Link('/rel');
+    $link->setRel('nofollow');
+    $this->assertEquals('<a href="/rel" rel="nofollow"></a>', $link->asHtml());
+  }
+
+  public function testDownload()
+  {
+    $link = new Link('/file.png');
+    $link->setDownload();
+    $this->assertEquals('<a href="/file.png" download></a>', $link->asHtml());
+    $link->setDownload('file');
+    $this->assertEquals(
+      '<a href="/file.png" download="file"></a>',
+      $link->asHtml()
+    );
+  }
+}
