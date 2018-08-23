001cd949ecd90e8a437c36204d6a4b393d5efd97
Brooke Bryan <brooke@bajb.net>
2018-08-23 14:34:32 +0100

    Add input

---
+++ src/Tags/Form/Input.php
@@ -0,0 +1,52 @@
+<?php
+namespace Packaged\Glimpse\Tags\Form;
+
+use Packaged\Glimpse\Tags\AbstractContentTag;
+
+class Input extends AbstractContentTag
+{
+  const TYPE_BUTTON = 'button';
+  const TYPE_CHECKBOX = 'checkbox';
+  const TYPE_COLOR = 'color';
+  const TYPE_DATE = 'date';
+  const TYPE_DATETIME_LOCAL = 'datetime-local';
+  const TYPE_EMAIL = 'email ';
+  const TYPE_FILE = 'file';
+  const TYPE_HIDDEN = 'hidden';
+  const TYPE_IMAGE = 'image';
+  const TYPE_MONTH = 'month';
+  const TYPE_NUMBER = 'number';
+  const TYPE_PASSWORD = 'password';
+  const TYPE_RADIO = 'radio';
+  const TYPE_RANGE = 'range';
+  const TYPE_RESET = 'reset';
+  const TYPE_SEARCH = 'search';
+  const TYPE_SUBMIT = 'submit';
+  const TYPE_TEL = 'tel';
+  const TYPE_TEXT = 'text';
+  const TYPE_TIME = 'time';
+  const TYPE_URL = 'url';
+  const TYPE_WEEK = 'week';
+
+  protected $_tag = 'input';
+
+  public function setType($type)
+  {
+    $this->setAttribute('type', $type);
+  }
+
+  public function getType()
+  {
+    $this->getAttribute('type');
+  }
+
+  public function setValue($value)
+  {
+    $this->setAttribute('value', $value);
+  }
+
+  public function getValue()
+  {
+    $this->getAttribute('value');
+  }
+}
