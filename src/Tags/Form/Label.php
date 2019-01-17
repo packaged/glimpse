d1f4ebf8cd70009652cd333c350bacd0825ad474
Brooke Bryan <brooke@bajb.net>
2019-01-17 10:03:56 +0000

    Label Element

---
+++ src/Tags/Form/Label.php
@@ -0,0 +1,38 @@
+<?php
+namespace Packaged\Glimpse\Tags\Form;
+
+use Packaged\Glimpse\Core\HtmlTag;
+
+class Label extends HtmlTag
+{
+  protected $_tag = 'label';
+  protected $_for;
+
+  public static function create($for = null)
+  {
+    /** @var Label $ele */
+    $ele = parent::create();
+    $ele->setFor($for);
+    return $ele;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFor()
+  {
+    return $this->_for;
+  }
+
+  /**
+   * @param mixed $for
+   *
+   * @return Label
+   */
+  public function setFor($for)
+  {
+    $this->_for = $for;
+    $this->setAttribute('for', $this->_for);
+    return $this;
+  }
+}
