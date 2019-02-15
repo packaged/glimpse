de676df298d67e8cf05c28266aa0926b28553ce9
Brooke Bryan <brooke@bajb.net>
2019-02-15 17:25:08 +0000

    No longer accurate text

---
+++ src/Tags/Text/NoLongerAccurateText.php
@@ -0,0 +1,9 @@
+<?php
+namespace Packaged\Glimpse\Tags\Text;
+
+use Packaged\Glimpse\Tags\AbstractContentTag;
+
+class NoLongerAccurateText extends AbstractContentTag
+{
+  protected $_tag = 's';
+}
